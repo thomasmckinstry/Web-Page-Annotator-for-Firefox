af85c7b411574b2674f1f77f71611593fa2f1b9e
Thomas McKinstry <thomas.g.mckinstry@protonmail.com>
2025-07-02 11:02:00 -0700

    Adjusted to use a Node.isEqualNode for comparisons

--- content_scripts/annotater.js
+++ content_scripts/annotater.js
@@ -6,16 +6,40 @@ function highlightTextReceived(request, sender, sendResponse) {
 
 function highlightText() {
     let selection = document.getSelection()
-    let node = selection.anchorNode
-    let nodeList = node.parentNode.childNodes
+    let anchor = selection.anchorNode
+    let nodeList = anchor.parentNode.childNodes
+
     let inSelection = false
+
+    let endSelection = selection.focusNode.cloneNode()
+    let startSelection = selection.anchorNode.cloneNode()
+    endSelection.innerHTML = selection.focusNode.innerHTML
+    startSelection.innerHTML = selection.anchorNode.innerHTML
+
+    if (selection.rangeCount > 1) {
+        console.log("Can only highlight one range at a time.")
+        return
+    }
+
+    if (selection.direction = "backward") {
+
+    }
+
+
+    if (endSelection.nodeType == Node.TEXT_NODE) {
+        endSelection = selection.focusNode.parentNode.cloneNode()
+        endSelection.innerHTML = selection.focusNode.parentNode.innerHTML
+        // console.log(selection.focusNode.parentNode, selection.focusNode.parentElement)
+    }
 
     var mark = document.createElement('mark');
 
+    // Gonna need to sub this out for a while look to properly iterate across multiple nodes.
     for (i = 0; i < nodeList.length; i++) {
-        node = nodeList[i]
-        if (node === selection.anchorNode || node === selection.focusNode.nextSibling) {
-            inSelection = !inSelection
+        let node = nodeList[i]
+        console.log(node)
+        if (node == selection.anchorNode) {
+            inSelection = true
         }
 
         if (inSelection) {
@@ -26,6 +50,13 @@ function highlightText() {
                 node.parentNode.removeChild(node)
                 i--;
             }
+        }
+
+        if (node.isEqualNode(endSelection)) {
+            inSelection = false
+            console.log(node, "===", endSelection)
+        } else {
+            console.log(node, "!=", endSelection)
         }
     }
 
