2cf98baa32650143ff0a8a41cc27b47dced2bc82
Thomas McKinstry <thomas.g.mckinstry@protonmail.com>
2025-07-15 16:39:23 -0700

    Added shortcut for open/close sidebar. Slight formatting changes to highighlightText. Added code to save an annotation to localStorage

--- content_scripts/annotater.js
+++ content_scripts/annotater.js
@@ -9,62 +9,51 @@ function highlightText() {
     let anchor = selection.anchorNode
     let nodeList = anchor.parentNode.childNodes
     let direction = selection.direction // Saving this so it can be used at the end. Removing selected nodes affects the selection, which may cause issues.
+    let id = getId()
 
     let inSelection = false
 
     // Cloning nodes as nodes will be removed as we iterate through the NodeList, so references may be lost otherwise.
     let endSelection = selection.focusNode.cloneNode()
     let startSelection = selection.anchorNode.cloneNode()
+
     endSelection.innerHTML = selection.focusNode.innerHTML
+    endSelection.outerHTML = selection.focusNode.outerHTML
     startSelection.innerHTML = selection.anchorNode.innerHTML
+    startSelection.outerHTML = selection.anchorNode.outerHTML
 
     // Highlighting multiple ranges would be complicated, and not really align with how I would expect people to use the extension.
     if (selection.rangeCount > 1) {
         console.log("Can only highlight one range at a time.")
+        window.alert("Can only highlight one range at a time.")
+        return
+    } else if (direction == "backward") {
+        console.log("Cannot annotate selections done 'backwards'")
+        window.alert("Cannot annotate selections done 'backwards'")
         return
     }
 
-    // The direction a selection is made in affects the anchor Node and focus Node. Doing this is easier than changing the direction of iteration.
-    if (direction == "backward") {
-        console.log("backwards!")
-        endSelection = selection.anchorNode.cloneNode()
-        endSelection.innerHTML = selection.anchorNode.innerHTML
-        startSelection = selection.focusNode.cloneNode()
-        startSelection = selection.focusNode.innerHTML
-    }
-
-    // This might be unnecessary after making the change to the comparison function.
-    // if (endSelection.nodeType == Node.TEXT_NODE) {
-    //     endSelection = selection.focusNode.parentNode.cloneNode()
-    //     endSelection.innerHTML = selection.focusNode.parentNode.innerHTML
-    // }
-
     var mark = document.createElement('mark');
+    mark.setAttribute("id", id)
     var selectionRemainder;
 
-    console.log("Anchor Node:", startSelection)
-    console.log("Focus Node:", endSelection)
-
-    // Gonna need to sub this out for a while look to properly iterate across multiple nodes.
-    // console.log(startSelection)
     for (i = 0; i < nodeList.length; i++) {
         let node = nodeList[i]
-        console.log(node)
         if (node.isEqualNode(startSelection)) {
             inSelection = true
-            console.log("inSelection true")
         }
 
         if (node.isEqualNode(endSelection) && node.nodeType == Node.TEXT_NODE) {
             let selectedText = node.textContent.substring(0, selection.focusOffset)
             selectionRemainder = document.createTextNode(node.textContent.substring(selection.focusOffset))
-            console.log(selectionRemainder.innerHTML)
             mark.innerHTML = mark.innerHTML + selectedText
             node.parentNode.removeChild(node)
         }
         else if (inSelection) {
             let newNode = node.cloneNode()
             newNode.innerHTML = node.innerHTML
+            newNode.outerHTML = node.outerHTML
+            console.log(newNode)
             mark.appendChild(newNode)
             if (!node.isEqualNode(startSelection) && node.parentNode != null) {
                 node.parentNode.removeChild(node)
@@ -74,21 +63,12 @@ function highlightText() {
 
         if (node.isEqualNode(endSelection)) {
             inSelection = false
-            console.log("inSelection false")
         }
     }
 
-    if (direction == "backwards") {
-        // console.log("Adding mark at focus",mark)
-        selection.focusNode.parentNode.replaceChild(mark, selection.focusNode)
-    } else {
-        // console.log("Adding mark at anchor",mark)
-        selection.anchorNode.parentNode.replaceChild(mark, selection.anchorNode)
-    }
-
-    console.log(selectionRemainder)
-    console.log("Place Remainder after mark", mark.nextSibling)
+    selection.anchorNode.parentNode.replaceChild(mark, selection.anchorNode)
     mark.parentNode.insertBefore(selectionRemainder, mark.nextSibling)
+    localStorage.setItem(id, `{ "type": "highlight" "selection": ${selection} "mark": ${mark} }`)
 }
 
 // Similar process to highlightTextReceived
@@ -98,6 +78,18 @@ function highlightText() {
 // An additional popup has to exist to receive the note text.
 function annotateTextReceived(request, sender, sendResponse) {
     let selection = document.getSelection()
+}
+
+function getId() {
+    let note = localStorage.getItem(0)
+    let id = 0;
+
+    while (note != null) {
+        id += 1
+        note = localStorage.getItem(id)
+    }
+
+    return id
 }
 
 browser.runtime.onMessage.addListener((command, tab) => {
