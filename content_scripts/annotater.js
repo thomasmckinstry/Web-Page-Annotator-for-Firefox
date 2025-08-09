0d1626cb58821193d274c9c716daefd76ac14242
Thomas McKinstry <thomas.g.mckinstry@protonmail.com>
2025-08-09 16:22:45 -0700

    Started implementing displaay notes

--- content_scripts/annotater.js
+++ content_scripts/annotater.js
@@ -63,8 +63,8 @@ function highlightText(id, start, end, startOffset, endOffset) {
     var startClone = start.cloneNode()
     var endClone = end.cloneNode()
     var mark = document.createElement("mark")
-    mark.setAttribute("annotaterId", id)
     if (start.isEqualNode(end) && start.nodeType == Node.TEXT_NODE) {
+        mark.setAttribute("annotaterId", id)
         var startText = document.createTextNode(startClone.textContent.substring(0, startOffset))
         var endText = document.createTextNode(endClone.textContent.substring(endOffset))
         var markText = document.createTextNode(startClone.textContent.substring(startOffset, endOffset))
@@ -76,16 +76,17 @@ function highlightText(id, start, end, startOffset, endOffset) {
     }
     var nodes = getNodesInRange(start, end)
     nodes.forEach((node) => {
-        mark = document.createElement("mark")
-        node.parentNode.replaceChild(mark, node)
+        if (node.isEqualNode(start) || !node.parentNode.isEqualNode(mark.parentNode)) {
+            mark = document.createElement("mark")
+            mark.setAttribute("annotaterId", id)
+            node.parentNode.replaceChild(mark, node)
+        }
         mark.appendChild(node)
         if (node.isEqualNode(startClone) && node.nodeType == Node.TEXT_NODE) {
-            console.log("Start offset", node, startOffset)
             var newText = document.createTextNode(node.textContent.substring(0, startOffset))
             node.textContent = node.textContent.substring(startOffset)
             mark.parentNode.insertBefore(newText, mark)
         } else if (node.isEqualNode(endClone) && node.nodeType == Node.TEXT_NODE) {
-            console.log("End offset", node, endOffset)
             var newText = document.createTextNode(node.textContent.substring(endOffset))
             node.textContent = node.textContent.substring(0, endOffset)
             mark.parentNode.replaceChild(newText, mark)
@@ -95,7 +96,7 @@ function highlightText(id, start, end, startOffset, endOffset) {
 }
 
 // Similar process to highlightTextReceived
-// Call a separate function annotateText(). Find the selection and place it all into a div.
+// Call a separate function annotateText(). Find the selection and place it all into a span.
 // The event listeners will be added to the element that is created upon the annotation.
 // Some style aspect should be changed to indicate that a section of text is annotated
 // An additional popup has to exist to receive the note text.
@@ -110,12 +111,17 @@ function annotateTextReceived(request, sender, sendResponse) {
     }
 
     var range = selection.getRangeAt(0)
-    // TODO: Actually implement the popup.
+    var start = range.startContainer
+    var startOffset = range.startOffset
+    var end = range.endContainer
+    var endOffset = range.endOffset
+    annotateText(id, start, end, startOffset, endOffset, note)
+    // TODO: This does not save a usable object, look into JSON.stringify()
     localStorage.setItem(`annotater${id}${url}`, `{ "type": "note", "range": "${range}", "annotation": "${note}", "startNode": "${range.startContainer}", "endNode":"${range.endContainer}", "startOffset":"${range.startOffset}", "endOffset":"${range.endOffset}"}`)
     if (localStorage.getItem("annotationCount") == null) {
         localStorage.setItem("annotationCount", 1)
     } else {
-        localStorage.setItem("annotationCount", localStorage.getItem("annotationCount") + 1)
+        localStorage.setItem("annotationCount", localStorage.getItem("annotationCount") + 1) // TODO: This adds 1 to a string
     }
     try {
         // TODO: range.toString() can return gibberish in certain cases (See phonetics) [Try changing the charset]
@@ -125,13 +131,73 @@ function annotateTextReceived(request, sender, sendResponse) {
     }
 }
 
+// Operates similarly to highlightText. Additional complication in that instead of <mark> selection must be placed in a <span> that has a script to popup the note that was made.
+function annotateText(id, start, end, startOffset, endOffset, note) {
+    var startClone = start.cloneNode()
+    var endClone = end.cloneNode()
+    var span = document.createElement("span")
+    var popup = document.createElement("span")
+    popup.textContent = note
+    popup.class = `annotater-popup${id}`
+    span.setAttribute("annotaterId", id)
+    if (start.isEqualNode(end) && start.nodeType == Node.TEXT_NODE) {
+        var startText = document.createTextNode(startClone.textContent.substring(0, startOffset))
+        var endText = document.createTextNode(endClone.textContent.substring(endOffset))
+        var spanText = document.createTextNode(startClone.textContent.substring(startOffset, endOffset))
+        span.appendChild(spanText)
+        start.parentNode.replaceChild(endText, start)
+        endText.parentNode.insertBefore(span, endText)
+        span.parentNode.insertBefore(startText, span)
+        popup.style.display = "none"
+        popup.style.color = "red" // TODO: Remove this
+        popup.style.position = "absolute"
+        span.appendChild(popup)
+        span.addEventListener("mouseenter", () => {
+            popup.style.display = "block"
+        });
+        span.addEventListener("mouseleave", () => {
+            popup.style.display = "none"
+        });
+        return   
+    }
+    var nodes = getNodesInRange(start, end)
+    nodes.forEach((node) => {
+        if (node.isEqualNode(start) || !node.parentNode.isEqualNode(span.parentNode)) {
+            span = document.createElement("span")
+            span.setAttribute("annotaterId", id)
+            node.parentNode.replaceChild(span, node)
+            span.addEventListener("mouseenter", () => {
+                popup.style.display = "block"
+            });
+            span.addEventListener("mouseleave", () => {
+                popup.style.display = "none"
+            })
+        }
+        span.appendChild(node)
+        if (node.isEqualNode(startClone) && node.nodeType == Node.TEXT_NODE) {
+            var newText = document.createTextNode(node.textContent.substring(0, startOffset))
+            node.textContent = node.textContent.substring(startOffset)
+            span.parentNode.insertBefore(newText, span)
+        } else if (node.isEqualNode(endClone) && node.nodeType == Node.TEXT_NODE) {
+            var newText = document.createTextNode(node.textContent.substring(endOffset))
+            node.textContent = node.textContent.substring(0, endOffset)
+            span.parentNode.replaceChild(newText, span)
+            newText.parentElement.insertBefore(span, newText)
+        }
+    })
+    popup.style.display = "none"
+    popup.style.color = "red" // TODO: Remove this
+    popup.style.position = "absolute" // TODO: Style this properly
+    span.appendChild(popup)
+}
+
 function getId() {
-    var note = localStorage.getItem(`annotater0`)
+    var note = localStorage.getItem(`annotater0${url}`)
     var id = 0;
 
     while (note != null) {
         id += 1
-        note = localStorage.getItem(`annotater${id}`)
+        note = localStorage.getItem(`annotater${id}${url}`)
     }
 
     return id
@@ -146,7 +212,7 @@ function refreshSidebar() {
             if (item.type == "note") {
                 browser.runtime.sendMessage({type: "annotate-text", id: key, content: item.content, annotation: item.note})
             } else {
-                browser.runtime.sendMessage({type: "highlight-text", id: key, content: item.range}) // TODO: Modify this to comply localStorage schema
+                browser.runtime.sendMessage({type: "highlight-text", id: key, content: item.range})
             }
         }
     }
@@ -181,7 +247,7 @@ function reannotate() {
             let note = JSON.parse(localStorage.getItem(key))
             if (note.type == "highlight") {
                 highlightText(note.id, note.start, note.startOffset, note.end, note.endOffset)
-                annotateText(note.id, note.start, note.startOffset, note.end, note.endOffset) // TODO: Actually implement annotateText
+                // annotateText(note.id, note.start, note.startOffset, note.end, note.endOffset) // TODO: Actually implement annotateText
             }
         }
         node = getNextNode(node)
