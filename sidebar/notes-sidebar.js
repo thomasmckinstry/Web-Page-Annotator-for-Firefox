ef6994b91e13c4e7c62c6a4082f84f8c3fd65cf9
Thomas McKinstry <thomas.g.mckinstry@protonmail.com>
2025-08-21 15:08:05 -0700

    Added function specs, made slight efficiency changes, commented code where it might be confusing

--- sidebar/notes-sidebar.js
+++ sidebar/notes-sidebar.js
@@ -10,6 +10,11 @@ let annotationColor = "#ff4500";
 let myWindowId
 let notes = document.querySelector(".notes-window")
 
+/**
+ * Creates the link button for the identified note in the sidebar.
+ * @param id the id of a note, in the format "annotater${id}/${url}"
+ * @returns an html <img> element containing the link icon and a listener that will tell the content_script where to scroll to.
+ */
 function createLinkButton(id) {
   let linkButton = document.createElement('img')
   linkButton.src = linkFilepath
@@ -20,27 +25,45 @@ function createLinkButton(id) {
   return linkButton
 }
 
+/**
+ * Creates the delete button for the identified note.
+ * @param id the id of a note, in the format "annotater${id}/${url}"
+ * @returns an html <img> element telling the content script which note to delete.
+ */
 function createDeleteButton(id) {
   let deleteButton = document.createElement('img')
   deleteButton.src = deleteFilepath
   deleteButton.className = "button"
   deleteButton.addEventListener("click", () => {
     handleMessage(id, "delete-note", "")
+    deleteNote(id)
   })
   return deleteButton
 }
 
+/**
+ * Creates the edit button for the identified annotated note.
+ * @param id the id of a note in the format "annotater${id}/${url}"
+ * @returns an html <img> element to edit the identified note and modify the localStorage for a page.
+ */
 function createEditButton(id) {
   let editButton = document.createElement('img')
   editButton.src = editFilepath
   editButton.className = "button"
   editButton.addEventListener("click", () => {
-    handleMessage(id, "edit-note", "")
+    let value = editNote(id)
+    handleMessage(id, "edit-note", value)
   })
   return editButton
 }
 
+/**
+ * Creates and inserts a <div> element to the sidebar containing the note given in the message.
+ * @param message The message sent by the content_script, containing type, id, start/endData, start/endOffset, and content attributes.
+ * @returns null
+ */
 function displayHighlight(message) {
+  // Checks to make sure that duplicate notes are not added.
   if (document.getElementById(message.id) != null) {
     return
   }
@@ -57,12 +80,19 @@ function displayHighlight(message) {
                         <p class="note-text">${message.content}</p>`
   notes.appendChild(newNote)
   let buttonsArray = document.getElementsByClassName("buttons")
+  // Finds the 'last' element of 'buttons' class in the DOM, this will find the one corresponding to the most recent note, as it will be the last one in the DOM.
   let buttons = buttonsArray[buttonsArray.length - 1]
   buttons.appendChild(linkButton)
   buttons.appendChild(deleteButton)
 }
 
+/**
+ * Creates and inserts a <div> element to the sidebar containing the note given in the message. Includes the text the user gave as an annotation.
+ * @param message The message sent by the content_script, containing type, id, start/endData, start/endOffset, annotation, and content attributes.
+ * @returns null
+ */
 function displayAnnotation(message) {
+  // Checks to make sure that duplicate notes aren't added.
   if (document.getElementById(message.id) != null) {
     return
   }
@@ -82,66 +112,72 @@ function displayAnnotation(message) {
                         <p class="note-text annotation-text">${message.annotation}</p>`
   notes.appendChild(newNote)
   let buttonsArray = document.getElementsByClassName("buttons")
+  // Finds the 'last' element of 'buttons' class in the DOM, this will find the one corresponding to the most recent note, as it will be the last one in the DOM.
   let buttons = buttonsArray[buttonsArray.length - 1]
   buttons.appendChild(linkButton)
   buttons.appendChild(deleteButton)
   buttons.appendChild(editButton)
 }
 
+/**
+ * Queries the content_script for the notes saved on a current page.
+ */
 function  refreshNotes() {
+  // Resets the sidebar, since the sidebar would stay open and retain notes between pages otherwise.
   while (notes.firstChild) {
     notes.removeChild(notes.firstChild);
   }
-
+  // The content_script will receive this message, and send all saved notes. There will be handled as normal by the listeners.
   handleMessage(null, "refresh-sidebar", "")
 }
 
+/**
+ * This function handles functionality that involves communication with the content_script and avoids repeating code.
+ * @param id the id of a note, in the format "annotater${id}/${url}"
+ * @param command the command to tell the content_script exactly what to do.
+ * @param value The relevant value for the content_script to use to execute the action (ex. the new annotation for a note)
+ */
 function handleMessage(id, command, value) {
-  switch (command) {
-    case "delete-note":
-      deleteNote(id)
-      break;
-    case "edit-note":
-      value = editNote(id)
-      break;
-  }
-
   let querying = browser.tabs.query({
   active: true,
   currentWindow: true,
   });
   querying.then(messageTab)
-
-  // Sends a message to the tab containing the name of the note and command
+  // Sends a message to the current (index 0) tab containing the name of the note, command, and relevant value
   function messageTab(tabs) {
     browser.tabs.sendMessage(tabs[0].id, {type: command, id: id, note: value})
   } 
 }
 
+/**
+ * Removes the identified note from the sidebar.
+ * @param id the id of a note, in the format "annotater${id}/${url}"
+ */
 function deleteNote(id) {
-  let notes = document.getElementsByClassName("note")
-  for (i = 0; i < notes.length; i++) {
-    if (notes[i].id == id) {
-      notes[i].parentElement.removeChild(notes[i])
-    }
-  }
+  let note = document.getElementById(id)
+  note.remove()
 }
 
+/**
+ * 
+ * @param id the id of a note, in the format "annotater${id}/${url}" 
+ * @returns null - Accounts for a user exiting without entering a new value.
+ * @returns The new annotation, to be sent to the content_script and put in localStorage.
+ */
 function editNote(id) {
   let newNote = prompt("Enter Note.")
   if (newNote == null || newNote == "") {
     return
   }
-  let annotations = document.getElementsByClassName("annotation-text")
-  for (i = 0; i < annotations.length; i++) {
-    if (annotations[i].parentElement.id === id) {
-      annotations[i].textContent = newNote
-      break;
-    }
-  }
+  let note = document.getElementById(id)
+  let annotation = note.getElementsByClassName("annotation-text")[0]
+  annotation.textContent = newNote
   return newNote
 }
 
+/**
+ * Adds all listeners to the sidebar.
+ */
 function addListeners() {
   // Update content when a new tab becomes active.
   browser.tabs.onActivated.addListener(refreshNotes);
@@ -149,6 +185,7 @@ function addListeners() {
   // Update content when a new page is loaded into a tab.
   browser.tabs.onUpdated.addListener(refreshNotes);
 
+  // Notifies content_script when color pickers change, and saves new colors to sidebar localStorage.
   let colorPickers = document.getElementsByClassName("color-picker")
   for (i = 0; i < colorPickers.length; i++) {
     colorPickers[i].addEventListener("change", (event) => {
@@ -174,6 +211,11 @@ function addListeners() {
   })
 }
 
+/**
+ * Saves any changes to the color pickers to localStorage.
+ * @param type indicates if a color corresponds to highlighted text or annotated text.
+ * @param value the hex code for the new color
+ */
 function saveColor(type, value) {
   switch (type) {
     case "highlight-color-change":
@@ -185,22 +227,22 @@ function saveColor(type, value) {
   }
 }
 
+/**
+ * Gets colors from localStorage, modifies the colorPickers as needed, and notifies the content_script.
+ */
 function loadColors() {
   highlightColor = localStorage.getItem("highlightColor")
   annotationColor = localStorage.getItem("annotationColor")
-
   if (highlightColor == null) {
-    highlightColor = "yellow";
-    localStorage.setItem("highlightColor", "yellow")
+    highlightColor = "#ffff00";
+    localStorage.setItem("highlightColor", "#ffff00")
   }
   if (annotationColor == null) {
     annotationColor = "#ff4500";
     localStorage.setItem("annotationColor", "#ff4500")
   }
-
   let highlightPicker = document.getElementById("highlight-color-change")
   let annotatePicker = document.getElementById("annotate-color-change")
-
   handleMessage(null, "highlight-color-change", highlightColor)
   handleMessage(null, "annotate-color-change", annotationColor)
   highlightPicker.value = highlightColor
